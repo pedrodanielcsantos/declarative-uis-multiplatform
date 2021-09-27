b452e505e34f6ace0d9f4c0f3ec24bce7b5fe1c4
Pedro Santos <pedrodanielcsantos@gmail.com>
2021-09-27 15:45:41 +0100

    Improve task layout composables

--- android/app/src/main/java/com/pedrosantos/declarativemultiplatformist/ui/widgets/TaskList.kt
+++ android/app/src/main/java/com/pedrosantos/declarativemultiplatformist/ui/widgets/TaskList.kt
@@ -1,51 +1,28 @@
 package com.pedrosantos.declarativemultiplatformist.ui.widgets
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.BottomSheetScaffold
-import androidx.compose.material.Button
-import androidx.compose.material.Checkbox
-import androidx.compose.material.Divider
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material.icons.outlined.KeyboardArrowUp
-import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.pedrosantos.declarativemultiplatformist.R
 import com.pedrosantos.declarativemultiplatformist.common.Task
 import com.pedrosantos.declarativemultiplatformist.ui.theme.Shapes
 import com.pedrosantos.declarativemultiplatformist.ui.viewmodel.TaskListViewModel
-import com.pedrosantos.declarativemultiplatformist.ui.viewmodel.TaskSubmitionError
 import com.pedrosantos.declarativemultiplatformist.ui.viewmodel.TaskSubmitionResult
 import kotlinx.coroutines.launch
-import java.util.Date
+import java.util.*
 
 @ExperimentalMaterialApi
 @Composable
@@ -75,8 +52,7 @@ fun TaskListScreen(
                 )
             },
             sheetShape = Shapes.medium,
-            sheetPeekHeight = 0.dp,
-            backgroundColor = MaterialTheme.colors.surface,
+            sheetPeekHeight = 0.dp
         ) {
             Column(
                 modifier = Modifier.fillMaxWidth(),
@@ -151,35 +127,45 @@ private fun TextIconButton(icon: ImageVector, text: String, action: ButtonAction
 
 @Composable
 private fun TaskList(tasks: List<Task>, onClick: (task: Task) -> Unit) {
-    LazyColumn {
-        items(tasks) { TaskCard(it, onClick) }
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 16.dp)
+    ) {
+        items(
+            items = tasks,
+            key = { it.hashCode() },
+        ) {
+            TaskCard(it, onClick)
+        }
     }
 }
 
 @Composable
 private fun TaskCard(task: Task, onClick: (task: Task) -> Unit) {
-    Column(
-        Modifier.clickable { onClick(task) }
-    ) {
-        Text(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            style = MaterialTheme.typography.body1,
-            text = "${task.content}!",
-            color = if (task.isUrgent) Color.Red else MaterialTheme.typography.body1.color
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            modifier = Modifier.padding(horizontal = 8.dp),
-            style = MaterialTheme.typography.caption,
-            text = "${Date(task.dueTimestamp)}",
-        )
-        Spacer(modifier = Modifier.height(2.dp))
-        Divider(
-            color = MaterialTheme.colors.secondary,
-            thickness = 1.dp,
-            modifier = Modifier.padding(horizontal = 8.dp)
-        )
-        Spacer(modifier = Modifier.height(2.dp))
+    Card(elevation = 4.dp) {
+        Column(
+            Modifier
+                .clickable { onClick(task) }
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                style = MaterialTheme.typography.body1,
+                text = task.content,
+                color = if (task.isUrgent) {
+                    MaterialTheme.colors.primary
+                } else {
+                    MaterialTheme.typography.body1.color
+                }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                style = MaterialTheme.typography.caption,
+                text = "${Date(task.dueTimestamp)}",
+            )
+        }
     }
 }
 
